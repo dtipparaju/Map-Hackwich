da09d88906c0e53afbaedc56b316ac16dd57b743
Dhanush Tipparaju <dhanushtips@gmail.com>
2023-02-09 09:46:53 -0600

    added user location

---
+++ Map Hackwich/LocationManager.swift
@@ -0,0 +1,19 @@
+//
+//  LocationManager.swift
+//  Map Hackwich
+//
+//  Created by Dhanush Tipparaju on 2/8/23.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
